8da8ef870cef281f88f7059a961bddc19816a35a
ShenJutsu <81960807+moritzstrobel@users.noreply.github.com>
2026-09-16 21:59:45 +0200

    Wire pistol conversion variants into unified generator

--- Python/generate_all_cfg.py
+++ Python/generate_all_cfg.py
@@ -26,6 +26,7 @@ from CFGGenerators.MachineGuns import generate_machine_gun_upgrades as machine_g
 from CFGGenerators.Pistols import generate_pistol_upgrades as pistol
 from CFGGenerators.Shotguns import generate_shotgun_upgrades as shotgun
 from CFGGenerators.SMGs import generate_smg_upgrades as smg
+from CFGGenerators.SMGs.pistol_conversion_variants import render_pistol_conversion_variant_patches
 from CFGGenerators.SMGs.smg_conversion_attachments import CONVERSION_ATTACHMENTS, attachment_block
 from CFGGenerators.Snipers import generate_sniper_upgrades as sniper
 from CFGGenerators.Common.shared_effects import render_shared_effects
@@ -233,6 +234,11 @@ def main():
     model, configs = build_model(apply_layout=False); dlc_models = build_dlc_outputs(model, configs); apply_layout_to_model(model); model.validate(); print(f"Built {model.summary()}")
     attachments = {sid: attachment_block(data) for sid, data in CONVERSION_ATTACHMENTS.items()}
     upgrade_text = render_consolidated_upgrade_prototypes(model); setup_text = render_final_general_setup_patch(model, attachments); npc_text = render_technician_patch(model); weapon_text = render_weapon_sections_patch(model)
+    variant_setup_text, variant_weapon_text = render_pistol_conversion_variant_patches(model)
+    if variant_setup_text:
+        setup_text = setup_text.rstrip() + "\n\n" + variant_setup_text
+    if variant_weapon_text:
+        weapon_text = weapon_text.rstrip() + "\n\n" + variant_weapon_text
     validate_rendered_outputs(model, upgrade_text, setup_text, npc_text)
     write(UPGRADES_PATH, upgrade_text); write(GENERAL_SETUP_PATH, setup_text); write(WEAPON_PATH, weapon_text); write(NPC_PATH, npc_text); write(VANILLA_COMPACTION_PATH, render_vanilla_compaction_patch())
     write(VANILLA_EFFECT_UI_PATH, render_vanilla_effect_ui_patch()); write(BPRUE_EFFECT_UI_PATH, render_bprue_effect_ui_patch()); _remove_independent_dlc_output()
