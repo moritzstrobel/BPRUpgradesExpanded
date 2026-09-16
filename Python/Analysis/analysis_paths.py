ff3bb9c103779bfa2da4a81ccd8736abec0cd814
ShenJutsu <81960807+moritzstrobel@users.noreply.github.com>
2026-09-16 21:29:28 +0200

    Register technician audit report path

--- Python/Analysis/analysis_paths.py
+++ Python/Analysis/analysis_paths.py
@@ -9,6 +9,7 @@ UPGRADE_MAP_COMPARISON = REPORTS_DIR / "upgrade_map_comparison.json"
 UPGRADE_LAYOUT_ANALYSIS = REPORTS_DIR / "upgrade_layout_analysis.json"
 WEAPON_UPGRADE_SECTIONS = REPORTS_DIR / "weapon_upgrade_sections.json"
 WEAPON_COVERAGE = REPORTS_DIR / "weapon_coverage.json"
+TECHNICIAN_UPGRADE_MAP = REPORTS_DIR / "technician_upgrade_map.json"
 
 
 def ensure_reports_dir() -> Path:
