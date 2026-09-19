d9eb22e501a6df44d713b8a8d4d4be7ac221cfaf
ShenJutsu <81960807+moritzstrobel@users.noreply.github.com>
2026-09-19 20:25:59 +0200

    Use custom pistol conversion module icon

--- Python/CFGGenerators/SMGs/generate_smg_upgrades.py
+++ Python/CFGGenerators/SMGs/generate_smg_upgrades.py
@@ -55,6 +55,7 @@ IMAGE = "Texture2D'/Game/GameLite/FPS_Game/UIRemaster/UITextures/PDA/Upgrades/We
 ICON = "Texture2D'/Game/GameLite/FPS_Game/UIRemaster/UITextures/PDA/Upgrades/Icons/T_PDA_Upgrades_Icon_Recoil.T_PDA_Upgrades_Icon_Recoil'"
 CALIBER_ICON = "Texture2D'/Game/GameLite/FPS_Game/UIRemaster/UITextures/PDA/Upgrades/Icons/T_PDA_Upgrades_Icon_CaliberChange.T_PDA_Upgrades_Icon_CaliberChange'"
 AMMO_ICON_ROOT = "/GameLite/FPS_Game/UIRemaster/UITextures/PDA/Upgrades/Ammo"
+PISTOL_CONVERSION_ICON = "Texture2D\'/GameLite/FPS_Game/UIRemaster/UITextures/PDA/Upgrades/Special/T_Module_Special_ConvertionkitBase.T_Module_Special_ConvertionkitBase\'"
 
 def _ammo_icon(name: str) -> str:
     asset = f"T_Module_Ammo_{name}"
@@ -240,7 +241,7 @@ def build_upgrades(config: dict) -> list[UpgradeDefinition]:
             current=caliber_sid(family,caliber); stat_effects=CALIBER_STAT_EFFECTS.get((source_caliber, caliber), ())
             upgrades.append(UpgradeDefinition(sid=current,general_setup_sid=family["general_setup_sid"],weapon_class="SMG",group="Caliber",target_part="Body",text_sid=data["name_sid"],hint_sid=data["hint_sid"],image=IMAGE,icon=CALIBER_ICON,cost=data["cost"],effects=(data["change_effect"],source_remove,data["add_ammo_effect"],*stat_effects),blocking_sids=tuple(x for x in conversion_sids if x!=current),template_sid=TEMPLATE_SID))
     for setup,(current,text,hint,cost) in PISTOL_CONVERSIONS.items():
-        upgrades.append(UpgradeDefinition(sid=current,general_setup_sid=setup,weapon_class="SMG",group="Conversion",target_part="Body",text_sid=text,hint_sid=hint,image=IMAGE,icon=CALIBER_ICON,cost=cost,template_sid=TEMPLATE_SID,require_effects=False,standalone=True))
+        upgrades.append(UpgradeDefinition(sid=current,general_setup_sid=setup,weapon_class="SMG",group="Conversion",target_part="Body",text_sid=text,hint_sid=hint,image=IMAGE,icon=PISTOL_CONVERSION_ICON,cost=cost,template_sid=TEMPLATE_SID,require_effects=False,standalone=True,preserve_icon=True))
     return upgrades
 
 def render_effects() -> str:
